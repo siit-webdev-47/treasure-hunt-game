4e62113596880c43a7f6d62a272da34c76f3bb0d
Ana-Maria Draganuta <anamariadraganuta95@gmail.com>
2024-07-12 19:33:51 +0300

    change const  energy

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,49 +1,58 @@
 import { useState } from "react";
-import './Player.css'; 
-
+import "./Player.css";
 
 function Player() {
-    
-    const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
-    const [energy, setEnergy] = useState(Math.floor(Math.random() * 50) + 25);
+  const [energy, setEnergy] = useState(Math.floor(Math.random() * 50) + 25);
 
-    const energyColor = (energy) => {
-        if (energy > 50) return  "green" ;
-        if (energy >= 25 && energy <= 50) return "yellow";
-        return  "red";
-      };
-   
-    return (
-        <>
-            <h2>Player component</h2>
-            <div>
-          
-                <label>
-                    Name : 
-                    <input 
-                        type="text" 
-                        value={name} 
-                        onChange={(e) => setName(e.target.value)} 
-                        className="nameAvatar"
-                    />
-                </label>
+  const energyColor = (energy) => {
+    if (energy > 50) {
+      return "green";
+    } else {
+      if (energy >= 25 && energy <= 50) {
+        return "yellow";
+      } else {
+        return "red";
+      }
+    }
+  };
 
-                {name && (
-                    <>
-                        <h3>{name}</h3>
-                        <img src={`https://api.dicebear.com/9.x/micah/svg?seed=${name}`} alt={`${name}'s player`} className="avatar" />
-                        <p className="energy" style={{ color: energyColor(energy) }}>
-                            Energy Level : {energy}
-                        </p>
-                        <button onClick={() => setEnergy(Math.floor(Math.random() * 50) + 25)} className="bottonEnergy">
-                           <span>Change Energy</span> 
-                        </button>
-                    </>
-                )}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <h2>Player component</h2>
+      <div>
+        <label>
+          Name :
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="nameAvatar"
+          />
+        </label>
+
+        {name && (
+          <>
+            <h3>{name}</h3>
+            <img
+              src={`https://api.dicebear.com/9.x/micah/svg?seed=${name}`}
+              alt={`${name}'s player`}
+              className="avatar"
+            />
+            <p className="energy" style={{ color: energyColor(energy) }}>
+              Energy Level : {energy}
+            </p>
+            <button
+              onClick={() => setEnergy(Math.floor(Math.random() * 50)+ 25)}
+              className="buttonEnergy">
+              <span>Change Energy</span>
+            </button>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
-export default Player
+export default Player;
