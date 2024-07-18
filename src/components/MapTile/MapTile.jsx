61473ecb6a06bac7e373ff547933fc88e41b1df2
Lucian <lucianicroitoru@gmail.com>
2024-07-19 02:02:54 +0300

    map tiles css changes

--- src/components/MapTile/MapTile.jsx
+++ src/components/MapTile/MapTile.jsx
@@ -2,20 +2,24 @@ import "./MapTile.css";
 
 function MapTile(props) {
   const { row, col, visited, requiredEnergy, yieldValue } = props.data;
-  const tileClass = visited ? 'visited' : 'unvisited';
+  const tileClass = visited ? "visited" : "unvisited";
+  const yieldValueEmojiClass =
+    yieldValue > 0 ? "yield-value-positive" : "yield-value-negative";
 
   return (
     <div className={`map-tile ${tileClass}`}>
       <div>
         <div className="tile-coordinates">
-          <small>Tile: {row},{col}</small>
+          <small>
+            Tile: {row},{col}
+          </small>
         </div>
       </div>
-      <div>⚡: {requiredEnergy}</div>
-      <div>🍀 : {yieldValue}</div>
-      <div className="tile-visited-property">
-        Visited: {String(visited)}
+      <div className="tile-text-size energy-emoji"> : {requiredEnergy}</div>
+      <div className={`tile-text-size ${yieldValueEmojiClass}`}>
+        : {yieldValue}
       </div>
+      <div className="tile-text-size">Visited: {String(visited)}</div>
     </div>
   );
 }
