95ec16d1fa42a4b11294fb7e6701b62d31974bf1
Gherasim Rares <rares.gherasim@gmail.com>
2024-07-20 22:47:10 +0300

    Created the folder Functions , the file generateMapTiles.js inside it and moved the coresponding functions in there

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
 import "./App.css";
+import generateMapTiles from "./components/Functions/generateMapTiles";
 import Map from "./components/Map/Map";
 import Player from "./components/Player/Player";
 import { createContext, useState, useEffect } from 'react';
 
 export const MapContext = createContext();
 
-function generateMapTiles(rows, cols) {
-  const tiles = [];
-  for (let i = 0; i < rows; i++) {
-    tiles[i] = [];
-    for (let j = 0; j < cols; j++) {
-      tiles[i][j] = {
-        row: i,
-        col: j,
-        visited: false,
-        requiredEnergy: Math.floor(Math.random() * 5 + 1),
-        yieldValue: Math.floor(Math.random() * 30 - 15),
-        hasTreasure: false,
-      };
-    }
-  }
-  generateTreasure(tiles, rows, cols);
-  return tiles;
-}
-
-function generateTreasure(tiles, rows, cols) {
-  let treasureRow = Math.floor(Math.random() * rows);
-  let treasureCol = Math.floor(Math.random() * cols);
-  tiles[treasureRow][treasureCol].hasTreasure = true;
-}
 
 
 
---
+++ src/components/Functions/generateMapTiles.js
@@ -0,0 +1,26 @@
+function generateMapTiles(rows, cols) {
+    const tiles = [];
+    for (let i = 0; i < rows; i++) {
+      tiles[i] = [];
+      for (let j = 0; j < cols; j++) {
+        tiles[i][j] = {
+          row: i,
+          col: j,
+          visited: false,
+          requiredEnergy: Math.floor(Math.random() * 5 + 1),
+          yieldValue: Math.floor(Math.random() * 30 - 15),
+          hasTreasure: false,
+        };
+      }
+    }
+    generateTreasure(tiles, rows, cols);
+    return tiles;
+  }
+  
+  function generateTreasure(tiles, rows, cols) {
+    let treasureRow = Math.floor(Math.random() * rows);
+    let treasureCol = Math.floor(Math.random() * cols);
+    tiles[treasureRow][treasureCol].hasTreasure = true;
+  }
+
+  export default generateMapTiles;
