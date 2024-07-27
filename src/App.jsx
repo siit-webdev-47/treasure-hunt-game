8bda9c49f495816c13d6a4de78e0e96715bc4dbb
Diana Papp <pappdiana148@gmail.com>
2024-07-27 13:54:59 +0300

    implement map rerendering based on players position changes

--- src/App.jsx
+++ src/App.jsx
@@ -28,14 +28,78 @@ function App() {
     }
   });
 
-  useEffect(() => {
-    const tileUpdate = setTimeout(() => {
-      map.tiles[0][3].visited = true;
-      setMap({ ...map, tiles: map.tiles });
-    }, 3000);
 
-    return () => clearTimeout(tileUpdate);
-  }, []);
+
+  useEffect(() => {
+    const handleKey = (event) => {
+      setMap((prevMap) => {
+        const { row, col } = prevMap.playerPosition;
+        let newRow = row;
+        let newCol = col;
+  
+        switch (event.key) {
+          case 'ArrowLeft':
+            newCol = col - 1;
+            if (newCol < 0) {
+              newCol = 0;  
+            }
+            
+            break;
+          case 'ArrowRight':
+            newCol = col + 1;
+            if (newCol >= cols) {
+              newCol = cols - 1;  
+            }
+           
+            break;
+          case 'ArrowUp':
+            newRow = row - 1;
+            if (newRow < 0) {
+              newRow = 0;  
+            }
+            
+            break;
+          case 'ArrowDown':
+            newRow = row + 1;
+            if (newRow >= rows) {
+              newRow = rows - 1;  
+            }
+            
+            break;
+          default:
+            return prevMap;  
+        }
+  
+        // map.tiles[newRow][newCol].visited = true;
+
+        const updatedTiles = prevMap.tiles.map((rowTiles, rowIndex) =>
+          rowTiles.map((tile, colIndex) => {
+            if (rowIndex === newRow && colIndex === newCol) {
+              return { ...tile, visited: true }; 
+            }
+            return tile;
+          })
+        );
+
+        return {
+          ...prevMap,
+          playerPosition: { 
+            row: newRow, 
+            col: newCol 
+          },
+          tiles: updatedTiles
+        };
+      });
+    };
+  
+    document.addEventListener('keyup', handleKey);
+  
+    return () => {
+      document.removeEventListener('keyup', handleKey);
+    };
+  }, [cols, rows]);
+  
+
   
   return (
     <>
@@ -49,3 +113,12 @@ function App() {
 }
 
 export default App;
+
+// useEffect(() => {
+//   const tileUpdate = setTimeout(() => {
+//     map.tiles[0][3].visited = true;
+//     setMap({ ...map, tiles: map.tiles });
+//   }, 3000);
+
+//   return () => clearTimeout(tileUpdate);
+// }, []);
