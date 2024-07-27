9046a066d1ce0d84d9d6dba4c4a0fd87d64ac544
Miana Neagos <miananeagos@gmail.com>
2024-07-27 18:30:05 +0300

    refactored App.jsx component and created custom hook usePlayerMovement to handle player movement logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import generateMapTiles from "./components/Functions/generateMapTiles";
 import Map from "./components/Map/Map";
 import Player from "./components/Player/Player";
 import { useState, useEffect, createContext } from "react";
+import usePlayerMovement from "./components/Custom-Hooks/usePlayerMovement";
 
 export const PlayerContext = createContext();
 
@@ -16,91 +17,20 @@ function App() {
     tiles: generateMapTiles(rows, cols),
     playerPosition: {
       row: 0,
-      col: 0
-    }
+      col: 0,
+    },
   });
 
-  const [player, setPlayer] = useState({ 
-    playerName: 'Rodica', 
-    playerEnergy: 15, 
-    get playerAvatar(){
-      return  `https://api.dicebear.com/9.x/micah/svg?seed=${this.playerName}` 
-    }
+  const [player, setPlayer] = useState({
+    playerName: "Rodica",
+    playerEnergy: 15,
+    get playerAvatar() {
+      return `https://api.dicebear.com/9.x/micah/svg?seed=${this.playerName}`;
+    },
   });
 
+  usePlayerMovement(setMap, rows, cols);
 
-
-  useEffect(() => {
-    const handleKey = (event) => {
-      setMap((prevMap) => {
-        const { row, col } = prevMap.playerPosition;
-        let newRow = row;
-        let newCol = col;
-  
-        switch (event.key) {
-          case 'ArrowLeft':
-            newCol = col - 1;
-            if (newCol < 0) {
-              newCol = 0;  
-            }
-            
-            break;
-          case 'ArrowRight':
-            newCol = col + 1;
-            if (newCol >= cols) {
-              newCol = cols - 1;  
-            }
-           
-            break;
-          case 'ArrowUp':
-            newRow = row - 1;
-            if (newRow < 0) {
-              newRow = 0;  
-            }
-            
-            break;
-          case 'ArrowDown':
-            newRow = row + 1;
-            if (newRow >= rows) {
-              newRow = rows - 1;  
-            }
-            
-            break;
-          default:
-            return prevMap;  
-        }
-  
-        // map.tiles[newRow][newCol].visited = true;
-
-        const updatedTiles = prevMap.tiles.map((rowTiles, rowIndex) =>
-          rowTiles.map((tile, colIndex) => {
-            if (rowIndex === newRow && colIndex === newCol) {
-              return { ...tile, visited: true }; 
-            }
-            return tile;
-          })
-        );
-
-        return {
-          ...prevMap,
-          playerPosition: { 
-            row: newRow, 
-            col: newCol 
-          },
-          tiles: updatedTiles
-        };
-      });
-    };
-  
-    document.addEventListener('keyup', handleKey);
-  
-    return () => {
-      document.removeEventListener('keyup', handleKey);
-    };
-  }, [cols, rows]);
-  
-
-  
   return (
     <>
       <h1>Treasure Hunt</h1>
@@ -114,11 +44,3 @@ function App() {
 
 export default App;
 
-// useEffect(() => {
-//   const tileUpdate = setTimeout(() => {
-//     map.tiles[0][3].visited = true;
-//     setMap({ ...map, tiles: map.tiles });
-//   }, 3000);
-
-//   return () => clearTimeout(tileUpdate);
-// }, []);
---
+++ src/components/Custom-Hooks/usePlayerMovement.jsx
@@ -0,0 +1,69 @@
+import { useEffect } from 'react';
+
+const usePlayerMovement = (setMap, cols, rows) => {
+  useEffect(() => {
+    const handleKey = (event) => {
+      console.log({event});
+      setMap((prevMap) => {
+        const { row, col } = prevMap.playerPosition;
+        let newRow = row;
+        let newCol = col;
+
+        switch (event.key) {
+          case "ArrowLeft":
+            newCol = col - 1;
+            if (newCol < 0) {
+              newCol = 0;
+            }
+            break;
+          case "ArrowRight":
+            newCol = col + 1;
+            if (newCol >= cols) {
+              newCol = cols - 1;
+            }
+            break;
+          case "ArrowUp":
+            newRow = row - 1;
+            if (newRow < 0) {
+              newRow = 0;
+            }
+            break;
+          case "ArrowDown":
+            newRow = row + 1;
+            if (newRow >= rows) {
+              newRow = rows - 1;
+            }
+            break;
+          default:
+            return prevMap;
+        }
+
+        const updatedTiles = prevMap.tiles.map((rowTiles, rowIndex) =>
+          rowTiles.map((tile, colIndex) => {
+            if (rowIndex === newRow && colIndex === newCol) {
+              return { ...tile, visited: true };
+            }
+            return tile;
+          })
+        );
+
+        return {
+          ...prevMap,
+          playerPosition: {
+            row: newRow,
+            col: newCol,
+          },
+          tiles: updatedTiles,
+        };
+      });
+    };
+
+    document.addEventListener("keyup", handleKey);
+
+    return () => {
+      document.removeEventListener('keyup', handleKey);
+    };
+  }, [setMap, cols, rows]);
+};
+
+export default usePlayerMovement;
