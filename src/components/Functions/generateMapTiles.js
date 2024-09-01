ecd74bb1357339b63f12d6db505edae0ef31fd04
Gherasim Rares <rares.gherasim@gmail.com>
2024-09-01 05:50:36 +0300

    Modified the algorithm to calculate the treasure possition, accordingly to the number of the tiles, the starting possition of the player and the minimum distance to the treasure

--- src/components/Functions/generateMapTiles.js
+++ src/components/Functions/generateMapTiles.js
@@ -19,12 +19,28 @@ function generateMapTiles(rows, cols, initRow = 0, initCol = 0) {
 
   function generateTreasure(tiles, rows, cols, initRow = 0, initCol = 0, initDistace = 3) {
     // TODO - implement the logic for any starting position, currently is implemented for starting position 0, 0 
-    let treasureRow = Math.floor(Math.random() * rows);
-    let minDiff = treasureRow < 3 ? 3 - treasureRow : 0;
-    let treasureCol = Math.floor(Math.random() * (cols - minDiff)) + minDiff;
+    let possibleTreasure = [];
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+
+        if ((Math.abs(initRow - i) + Math.abs(initCol - j)) > initDistace) 
+        possibleTreasure.push([i,j]);
+        
+      };
+      console.log(rows, cols);
+      
+    }
+    console.log(possibleTreasure);
+    
+    let treasurePos = Math.floor(Math.random() * possibleTreasure.length);
+    // let minDiff = treasureRow < 3 ? 3 - treasureRow : 0;
+    // let treasureCol = Math.floor(Math.random() * (cols - minDiff)) + minDiff;
 
 
-    tiles[treasureRow][treasureCol].hasTreasure = true;
+    // tiles[treasureRow][treasureCol].hasTreasure = true;
+    console.log(treasurePos,possibleTreasure[treasurePos][0],possibleTreasure[treasurePos][1]);
+    
+    tiles[possibleTreasure[treasurePos][0]][possibleTreasure[treasurePos][1]].hasTreasure = true;
   }
   
   export default generateMapTiles;
