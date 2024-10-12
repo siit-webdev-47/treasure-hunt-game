9d05ef62f64a1257d6e9752c69055bff4669848e
Gherasim Rares <rares.gherasim@gmail.com>
2024-10-12 17:26:01 +0300

    hided the tile's containments if it's not visible and also the treasure border if not visible

--- src/components/MapTile/MapTile.jsx
+++ src/components/MapTile/MapTile.jsx
@@ -1,19 +1,28 @@
 import "./MapTile.css";
 import { useContext } from "react";
 import { AppSettingsContext } from "../../App";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import energyLevel from "../Functions/energyLevel";
 
 function MapTile(props) {
-  const { row, col, visited, visible, requiredEnergy, yieldValue, hasTreasure } = props.mapTileData;
+  const {
+    row,
+    col,
+    visited,
+    visible,
+    requiredEnergy,
+    yieldValue,
+    hasTreasure,
+  } = props.mapTileData;
   const playerPosition = props.playerPosition;
 
   const { player } = useContext(AppSettingsContext);
 
-  const tileClass = visited ? 'visited' : 'unvisited';
-  const treasureIconClass = hasTreasure ? 'treasure-icon' : '';
-  const treasureTileClass = hasTreasure ? 'treasure-tile' : '';
-  const yieldValueEmojiClass = yieldValue >= 0 ? 'yield-value-positive' : 'yield-value-negative';
+  const tileClass = visited ? "visited" : "unvisited";
+  const treasureIconClass = hasTreasure && visible ? "treasure-icon" : "";
+  const treasureTileClass = hasTreasure && visible ? "treasure-tile" : "";
+  const yieldValueEmojiClass =
+    yieldValue >= 0 ? "yield-value-positive" : "yield-value-negative";
 
   const playerOnTile = playerPosition.row === row && playerPosition.col === col;
 
@@ -25,17 +34,31 @@ function MapTile(props) {
             Tile: {row},{col}
           </small>
         </div>
-        {playerOnTile && <img
-          src={player.playerAvatar}
-          alt={`${player.playerName}'s player`}
-          className={`tile-avatar ${energyLevel(player.playerEnergy)}`}
-        />}
+        {playerOnTile && (
+          <img
+            src={player.playerAvatar}
+            alt={`${player.playerName}'s player`}
+            className={`tile-avatar ${energyLevel(player.playerEnergy)}`}
+          />
+        )}
       </div>
-      <div className="tile-text-size energy-emoji"> : {requiredEnergy}</div>
-      <div className={`tile-text-size ${yieldValueEmojiClass}`}> : {yieldValue}</div>
-      {/* <div className="tile-text-size">Visited: {String(visited)}</div> */}
-      <div className="tile-text-size">Visible: {String(visible)}</div>
-      <div className={`${treasureIconClass}`}></div>
+      {/* <div> */}
+        {visible && (
+          <>
+            <div className="tile-text-size energy-emoji">
+              {" "}
+              : {requiredEnergy}
+            </div>
+            <div className={`tile-text-size ${yieldValueEmojiClass}`}>
+              {" "}
+              : {yieldValue}
+              <div className="tile-text-size">Visited: {String(visited)}</div>
+              <div className="tile-text-size">Visible: {String(visible)}</div>
+              <div className={`${treasureIconClass}`}></div>
+            </div>
+          </>
+        )}
+      {/* </div> */}
     </div>
   );
 }
@@ -44,5 +67,5 @@ export default MapTile;
 
 MapTile.propTypes = {
   mapTileData: PropTypes.any,
-  playerPosition: PropTypes.any
-}
+  playerPosition: PropTypes.any,
+};
