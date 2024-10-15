ae7103ee21324bc3a4786fbdcec726bfffd7b390
Miana Neagos <miananeagos@gmail.com>
2024-10-15 15:06:14 +0300

    Replaced Array.map() with direct row/column access for efficient tile updates - in Game component

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -18,17 +18,19 @@ function Game({ onPlayerMove }) {
 
 
     if (player.playerEnergy > 0 && newPlayerEnergy > 0 && !map.tiles[row][col].hasTreasure) {      
-       setMap((prevMap) => {        
-          const updatedTiles = prevMap.tiles.map((rowTiles, rowIndex) =>
-            rowTiles.map((tile, colIndex) => {
-              if (
-                (rowIndex === row && colIndex === col) ||  (rowIndex === newRow && colIndex === newCol)
-              ) {
-                return { ...tile, visited: true, yieldValue: 0};
-              }
-              return tile;
-            })
-          );
+       setMap((prevMap) => {   
+        const updatedTiles = [...prevMap.tiles] 
+        
+        if (updatedTiles[row] !== updatedTiles[newRow]) {
+          console.log('ROW & COL are different');
+          
+          updatedTiles[row] = [...updatedTiles[row]]
+          updatedTiles[newRow] = [...updatedTiles[newRow]]
+        }
+
+        updatedTiles[row][col] = { ...updatedTiles[row][col], visited: true, yieldValue: 0 };
+        updatedTiles[newRow][newCol] = { ...updatedTiles[newRow][newCol], visited: true, yieldValue: 0 };
+
           return{
             ...prevMap,
             playerPosition: {
@@ -67,3 +69,15 @@ export default Game;
 Game.propTypes = {
   onPlayerMove: PropTypes.func.isRequired,
 };
+
+
+// const updatedTiles = prevMap.tiles.map((rowTiles, rowIndex) =>
+//   rowTiles.map((tile, colIndex) => {
+//     if (
+//       (rowIndex === row && colIndex === col) ||  (rowIndex === newRow && colIndex === newCol)
+//     ) {
+//       return { ...tile, visited: true, yieldValue: 0};
+//     }
+//     return tile;
+//   })
+// );
