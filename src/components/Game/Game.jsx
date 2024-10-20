6cf0f77671200ae5209a30ccd42da56f3ebb82be
Adrian-Moldovan <ma.moldovan@gmail.com>
2024-10-20 10:48:06 +0300

    map update on player move

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -20,15 +20,6 @@ function Game({ onPlayerMove }) {
     if (player.playerEnergy > 0 && newPlayerEnergy > 0 && !map.tiles[row][col].hasTreasure) {      
        setMap((prevMap) => {   
         const updatedTiles = [...prevMap.tiles] 
-        
-        if (updatedTiles[row] !== updatedTiles[newRow]) {
-          console.log('ROW & COL are different');
-          
-          updatedTiles[row] = [...updatedTiles[row]]
-          updatedTiles[newRow] = [...updatedTiles[newRow]]
-        }
-
-        updatedTiles[row][col] = { ...updatedTiles[row][col], visited: true, yieldValue: 0 };
         updatedTiles[newRow][newCol] = { ...updatedTiles[newRow][newCol], visited: true, yieldValue: 0 };
 
           return{
@@ -69,15 +60,3 @@ export default Game;
 Game.propTypes = {
   onPlayerMove: PropTypes.func.isRequired,
 };
-
-
-// const updatedTiles = prevMap.tiles.map((rowTiles, rowIndex) =>
-//   rowTiles.map((tile, colIndex) => {
-//     if (
-//       (rowIndex === row && colIndex === col) ||  (rowIndex === newRow && colIndex === newCol)
-//     ) {
-//       return { ...tile, visited: true, yieldValue: 0};
-//     }
-//     return tile;
-//   })
-// );
