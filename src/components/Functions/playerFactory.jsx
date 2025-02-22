3b162b2c10e957da1fb7e98c6025078f3516ba3a
Gherasim Rares <rares.gherasim@gmail.com>
2025-02-23 01:26:28 +0200

    #43 Add playerResponses object to track response numbers

--- src/components/Functions/playerFactory.jsx
+++ src/components/Functions/playerFactory.jsx
@@ -1,6 +1,14 @@
 const playerFactory = (playerName = "Anony Moose", playerEnergy = 15) => ({
     playerName,
     playerEnergy,
+    playerResponses:{
+      easyCorrect: 0,
+      easyWrong: 0,
+      mediumCorrect: 0,
+      mediumWrong: 0,
+      hardCorrect: 0,
+      hardWrong: 0,
+    },
     get playerAvatar() {
       return `https://api.dicebear.com/9.x/micah/svg?seed=${this.playerName}`;
     }
