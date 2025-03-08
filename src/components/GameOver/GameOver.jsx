2dd764129f919a766b23d6fa77d06b3536892d6c
Gherasim Rares <rares.gherasim@gmail.com>
2025-03-08 19:09:49 +0200

    Refactored the GameOverStatistics, created the functions for calculating the scores and added some pretty effect for Adi

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -1,46 +1,15 @@
-import { useContext } from "react";
 import "./GameOver.css";
 import PropTypes from "prop-types";
-import { AppSettingsContext } from "../../App";
+import GameOverStatistics from "./GameOverStatistics";
 
 export default function GameOver({ newGame, resetGame, gameOverMsg }) {
-  const { player } = useContext(AppSettingsContext);
-  //  Create an array with the values of the playerResponses object
-  //  We know the exact order of the values
-  let playerResponsesArray = Object.values(player.playerResponses);
-  let averageSum =
-    (playerResponsesArray[0] + playerResponsesArray[1]) * 0.33 +
-    (playerResponsesArray[2] + playerResponsesArray[3]) * 0.66 +
-    (playerResponsesArray[4] + playerResponsesArray[5]) * 1;
-  let totalSumResponses = playerResponsesArray.reduce(
-    (sum, curr) => sum + curr,
-    0
-  );
-  let difficultyIndex = (averageSum / totalSumResponses).toFixed(2);
-  let finalScore =
-    playerResponsesArray[0] * 2 -
-    playerResponsesArray[1] * 3 +
-    playerResponsesArray[2] * 4 -
-    playerResponsesArray[3] * 2 +
-    playerResponsesArray[4] * 6 -
-    playerResponsesArray[5] * 1;
-
+  
   return (
     <div className="game-over-wrapper">
       <div className="game-over-container">
         <p>Game Over!</p>
         <p>{gameOverMsg}</p>
-        <div className="game-over-statistics">
-          <h3>Difficulty Index: {difficultyIndex * 100}%</h3>
-          <h3>Final Score: {finalScore}</h3>
-          <br />
-          <span>Correct Easy Answers: {playerResponsesArray[0]}</span>
-          <span>Wrong Easy Answers: {playerResponsesArray[1]}</span>
-          <span>Correct Medium Answers: {playerResponsesArray[2]}</span>
-          <span>Wrong Medium Answers: {playerResponsesArray[3]}</span>
-          <span>Correct Hard Answers: {playerResponsesArray[4]}</span>
-          <span>Wrong Hard Answers: {playerResponsesArray[5]}</span>
-        </div>
+        <GameOverStatistics />
         <div className="game-over-buttons">
           <button onClick={resetGame} className="reset round">
             Reset Round
