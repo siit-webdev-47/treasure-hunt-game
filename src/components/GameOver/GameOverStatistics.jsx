e7e4174ca8d1d76f0ad721efb580421609e36c04
Gherasim Rares <rares.gherasim@gmail.com>
2025-03-23 00:57:23 +0200

    Added the total correct and wrong  questions in statistics page

--- src/components/GameOver/GameOverStatistics.jsx
+++ src/components/GameOver/GameOverStatistics.jsx
@@ -35,6 +35,7 @@ export default function GameOverStatistics() {
             <th>Easy</th>
             <th>Medium</th>
             <th>Hard</th>
+            <th>TOTAL</th>
           </tr>
         </thead>
         <tbody>
@@ -43,12 +44,18 @@ export default function GameOverStatistics() {
             <td>{player.playerResponses.easyCorrect}</td>
             <td>{player.playerResponses.mediumCorrect}</td>
             <td>{player.playerResponses.hardCorrect}</td>
+            <td>{player.playerResponses.easyCorrect 
+            + player.playerResponses.mediumCorrect
+            + player.playerResponses.hardCorrect}</td>
           </tr>
           <tr>
             <td>Wrong</td>
             <td>{player.playerResponses.easyWrong}</td>
             <td>{player.playerResponses.mediumWrong}</td>
             <td>{player.playerResponses.hardWrong}</td>
+            <td>{player.playerResponses.easyWrong
+              + player.playerResponses.mediumWrong
+              + player.playerResponses.hardWrong}</td>
           </tr>
         </tbody>
       </table>
